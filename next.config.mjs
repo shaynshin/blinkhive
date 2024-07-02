903daba5c84a9c7036d9adf47722edc2639d26f0
shaynshin <shayn_shin@outlook.com>
2024-07-02 11:44:33 +0800

    redirect buy flow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -10,8 +12,20 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/actions.json',
-        destination: '/api/action/json',
+        source: "/actions.json",
+        destination: "/api/action/json",
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "/buy/:path*",
+        destination:
+          "https://actions.dialect.to/?action=solana-action:" +
+          baseUrl +
+          "/api/action/:path*",
+        permanent: true,
       },
     ];
   },
