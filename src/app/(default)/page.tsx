b5d9a05faf4017e09937208e8311c3dfd131b42d
shaynshin <shayn_shin@outlook.com>
2024-07-06 22:21:44 +0800

    adjust

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <section>
       <div className="pt-32 pb-12 md:pt-44 md:pb-20 min-h-[calc(100vh-4rem)] flex justify-center items-center">
-        <div className="px-4 sm:px-6 -mt-12">
+        <div className="px-4 sm:px-6 -mt-20">
           <PageHeader
             className="mb-12"
             title="shilling reimagined."
